0a037402c1a3cd221b592248d852f72bfa3e49a9
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-01-12 20:27:39 -0800

    Make and implement migrations for quizzes, questions and answers table

--- resources/js/Pages/Quizzes/Show.vue
+++ resources/js/Pages/Quizzes/Show.vue
@@ -1,6 +1,8 @@
 <script setup>
-
+defineProps({
+    id: Number,
+});
 </script>
 <template>
-    
+    <div>grab the quiz id from url then make a call to API -> then database and then start the quiz. {{  id  }}</div>
 </template>
