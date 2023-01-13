269b09ec894ab03887380d25bc4d34fe99f8dd46
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-01-12 17:01:27 -0800

    Add flowbite and update home page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Quizzes', [
+    return Inertia::render('Home', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
